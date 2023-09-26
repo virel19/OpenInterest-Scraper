1dae73fe5d41577362c41f11f3e1b6ce2b6ae68d
Virel <virel19@gmail.com>
2023-09-26 09:09:50 -0700

    Added args

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ async function run() {
         // Launch browser
         const browser = await puppeteer.launch({
             headless: 'new',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--disable-gpu',
+            ],
         });
         const page = await browser.newPage();
         await page.goto('https://app.gmx.io/#/dashboard', { waitUntil: 'networkidle2' });
